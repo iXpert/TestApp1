a8744764b03e18f63209298c1dc91b09c62f13de
Amr Elnagar <amr.elnagar@gmail.com>
2024-11-08 14:22:33 +0100

    Initial Commit

---
+++ TestApp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestApp1
+//
+//  Created by Amr Elnagar on 08/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestApp1/TestApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp1App.swift
+//  TestApp1
+//
+//  Created by Amr Elnagar on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
